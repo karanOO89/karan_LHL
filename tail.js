aff0c2009e10e12999511ad3cf924560430f12ca
karanOO89 <gk.987680@gmail.com>
2021-03-30 00:30:17 +0000

    tail.js added

--- tail.js
+++ tail.js
@@ -1,18 +1,16 @@
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅✅✅ ${actual} === ${expected}`);
-    } else {
-      console.assert(actual === expected, `🛑🛑🛑 ${actual} !== ${expected}`);
-    }
-  };
-const tail = function(arr){
-    let m=[];
-  for(i=1;i<arr.length;i++){
-  
-    
+  if (actual === expected) {
+    console.log(`✅✅✅ ${actual} === ${expected}`);
+  } else {
+    console.assert(actual === expected, `🛑🛑🛑 ${actual} !== ${expected}`);
+  }
+};
+const tail = function(arr) {
+  let m = [];
+  for (let i = 1; i < arr.length; i++) {
     m.push(arr[i]);
   }
-  return(m);
+  return (m);
 };
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result[0],"Lighthouse"); // => will always fail!
