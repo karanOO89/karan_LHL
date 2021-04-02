4880cfea6182029c808a5daa658c827949a3e078
karanOO89 <gk.987680@gmail.com>
2021-04-02 01:10:22 +0000

    findkey.js added

---
+++ findKey.js
@@ -0,0 +1,32 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ ${actual} === ${expected}`);
+  } else {
+    console.assert(actual === expected, `🛑🛑🛑 ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (obj, callback) => {
+  const keys = Object.keys(obj);
+
+  for (let ele of keys) {
+    if (callback(obj[ele])) {
+      return ele;
+    }
+  }
+  return;
+};
+
+const y = findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+); // => "noma"
+
+console.log(y);
