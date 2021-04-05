c6163830e0af2f0b3ab658943169d3d6ff007446
karanOO89 <gk.987680@gmail.com>
2021-04-05 23:42:56 +0000

    mocha/chai implementations on tests

--- eqArrays_withRecurrsion.js
+++ eqArrays_withRecurrsion.js
@@ -1,19 +1,20 @@
 const eqArrays = (arr1, arr2) => {
-  x = false;
-  if (arr1.length !== arr2.length) return x;
+    if (arr1.length !== arr2.length) return false;
 
   for (let i = 0; i <= arr1.length - 1; i++) {
     if (!Array.isArray(arr1[i]) || !Array.isArray(arr2[i])) {
+     
       if (arr1[i] !== arr2[i]) {
-        console.log(arr1[i], arr2[i]);
-        return x;
+        console.log(false);
+        return false;
       }
     } else {
-      eqArrays(arr1[i], arr2[i]);
-    }
+      // console.log(arr1[i], arr2[i]);
+      eqArrays(arr1[i], arr2[i])};
+    
   }
-  x = true;
-  return x;
+  
+  return true;
 };
 
 console.log(eqArrays([[2, 3], [4]], [[2, 3], [5]]));
